001f8da788a64270ef4fc833acc6f9556ff79b59
kevindanglu <kevindanglu@163.com>
2018-12-31 22:23:30 +0800

    Initialize the project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # maven-plugin-collector
 It's a project to collect maven plugins that are often used.
+
+
+Practice 1: switch ENV configuration through <profiles><profile></profile></profiles>
+
+maven command: mvn install -P test/prod
+
---
+++ src/main/java/com/danglu/plugin/PluginCollector.java
@@ -0,0 +1,16 @@
+package com.danglu.plugin;
+
+/**
+ * @ClassName: PluginCollector
+ * @Description: maven plugin collector
+ * @Author: Dang Lu
+ * @Date: 2018/12/31 21:55
+ * @Version: v1.0
+ **/
+
+public class PluginCollector {
+
+    public static void main(String[] args) {
+        System.out.println("This is a plugin collector");
+    }
+}
